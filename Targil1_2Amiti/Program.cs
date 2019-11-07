7684e0ec33d97646b9d92f97a1e14c8de782b751
eliezer gensburger <eliezer.jct@gmail.com>
2019-11-07 15:16:33 +0200

    va te faire voir

---
+++ Targil1_2Amiti/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil1_2Amiti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = -1;
+            do
+            {
+                switch (choice)
+                {
+                    case 0:
+                        //TO DO
+                        break;
+                    case 1:
+                        //TOO DO IT
+                        break;
+                    case 2:
+                        //FINISH?
+                        break;
+                    case 3:
+                        //ENFOIRE
+                        break;
+                    default:
+                        Console.WriteLine("error");
+                        break;
+                }
+            } while (choice !=0);
+            //TO DO
+        }
+    }
+}
